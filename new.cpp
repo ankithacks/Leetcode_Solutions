081f10ad3f0dc7d00b0db554654349cceb31ed8d
ankit-chakraborty <ankitguitar117@gmail.com>
2024-01-01 01:04:05 +0530

    first file

---
+++ new.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout<< "enter what you want to print:  "<<endl;
+    cin>> str;
+    cout<< str<< endl;
+    return 0;
+}
